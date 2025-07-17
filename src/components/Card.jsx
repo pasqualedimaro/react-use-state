4c75f13951df4933729bb1421c7d9971ef2081a4
pasqualedimaro <pakydm15@gmail.com>
2025-07-17 17:21:34 +0200

    add logic button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import languages from "../data/languages"
 
-function Cardfaq() {   
+function Cardfaq() {
+    const [title, setTitle] = useState(null)
+
     return(
         languages.map((language)=> (
-            <button>
-                {language.title}
-            </button>
+            <>
+            <div key={language.id}>
+                <button onClick={()=> setTitle(language.id)}>
+                    {language.title}
+                </button>
+                {title === language.id && (<p>{language.description}</p>)}
+
+            </div>
+            </>
         ))
     )
 }
